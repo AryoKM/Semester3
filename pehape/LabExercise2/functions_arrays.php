c4557423bd2e43b2befe9136d878a029fbd00a72
AryoKM <148169550+AryoKM@users.noreply.github.com>
2024-10-03 10:42:34 +0800

    Update functions_arrays.php

--- pehape/LabExercise2/functions_arrays.php
+++ pehape/LabExercise2/functions_arrays.php
@@ -1,18 +1,21 @@
 <?php
-include 'variables.php';
+function output() {
+    include 'variables.php';
 
-$students = [$name, "Feixiao", "Topaz", "Aventurine", "Robin"];
+    $students = [$name, "Feixiao", "Topaz", "Aventurine", "Robin"];
 
-echo $name . "<br>";
-echo $age . "<br>";
-echo $sex . "<br>";
-echo $gpa . "<br>";
-echo $isStudent ? "yes<br>" : "no<br>"; // pake ternary untuk ubah output dri boolean jdi string
+    echo $name . "<br>";
+    echo $age . "<br>";
+    echo $sex . "<br>";
+    echo $gpa . "<br>";
+    echo $isStudent ? "yes<br>" : "no<br>"; // pake ternary untuk ubah output dri boolean jdi string
 
-include "control_flow.php";
+    include "control_flow.php";
 
-function greet($name) {return "Hello " . $name;}
+    function greet($name) {return "Hello " . $name;}
 
-foreach ($students as $student) {echo greet($student) . "<br>";}
+    foreach ($students as $student) {echo greet($student) . "<br>";}
+}
 
+output();
 ?>
